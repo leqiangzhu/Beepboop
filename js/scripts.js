39f9f6816e585400cd75a7b271309289beb108dc
Leqiangzhu（David） <41922877+leqiangzhu@users.noreply.github.com>
2018-08-18 01:25:06 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-
+//the reverse is  not ready
+//this function is to check one single number to be which output
 function beebBoop(number){
+	
 	var numberString=number.toString();
 	var outputString;
 	var hasOne=numberString.includes("1");
@@ -7,11 +9,7 @@ function beebBoop(number){
 	var inByThree = number%3===0; 
 	
 	
-	/* if(!hasOne||!hasZero){
-		
-	} */
-	
-			if(inByThree && number!==0){
+			if(inByThree&&number!==0){
 			outputString="I'm sorry, Dave. I'm afraid I can't do that.";
 			
 			//	alert("I cant")
@@ -28,8 +26,8 @@ function beebBoop(number){
 	
 }
 
-
-function getArray(number){
+//the function to get the array
+ function getArray(number){
 	var outArray=[];
 
 			for (var i=0;i<=number;i++){
@@ -38,38 +36,35 @@ function getArray(number){
 			} 
 			
 	return outArray;
-}
+} 
 
+
+		 	function outputResult(array) {
+			  $("#output").text("");
+			  var index = 0;
+			  array.forEach(function(word){
+				$("#output").append(word + "<br>");
+				index++;
+			  });
+			  $("#results").slideDown();
+			} 
+	 
+ 
+ 
 
 $(document).ready(function() {
 
   $("#input-form").submit(function(event) {
+	   event.preventDefault();
 	var inputNum=$("#numInput").val()
 	var toOutputs = getArray(inputNum);
-  $('#output').text(toOutputs);
+	
+
+	outputResult(toOutputs);
+   
     $("#result").show();
-    event.preventDefault();
+
+
+});
+
   });
-}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
